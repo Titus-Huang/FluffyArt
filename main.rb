39e861864ceee82ed90d682133a9ccc63d26a95f
Titus Huang <titus.the.huang@gmail.com>
2022-09-13 11:50:56 +0930

    Added gitignore and preparing to deploy to Heroku

--- main.rb
+++ main.rb
@@ -1,11 +1,9 @@
-     
 require 'sinatra'
+require 'httparty'
+require 'dotenv/load'
+
 
 get '/' do
+  HTTParty.get("https://ombdapi.com?apikey=#{ENV['OMDB_API_KEY']}&t=jaws").to_s
   erb :index
 end
-
-
-
-
-
