4e30d37f2ddb4eef305b6d0746f8e5aded306ea2
Titus Huang <titus.the.huang@gmail.com>
2022-09-13 15:31:08 +0930

    Start working on databases and site name variable

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+
+-- Create database
+CREATE DATABASE fluffyart_db;
+
+-- Create Users table
+CREATE TABLE members(
+
+);
+
+-- Create Content table
+
--- main.rb
+++ main.rb
@@ -2,8 +2,13 @@ require 'sinatra'
 require 'httparty'
 require 'dotenv/load'
 
-
 get '/' do
-  HTTParty.get("https://ombdapi.com?apikey=#{ENV['OMDB_API_KEY']}&t=jaws").to_s
-  erb :index
+    # HTTParty.get("https://ombdapi.com?apikey=#{ENV['OMDB_API_KEY']}&t=jaws").to_s
+
+    page_title = "test title"
+
+    page_title = 
+    erb :index, locals: {
+        page_title: page_title
+    }
 end
