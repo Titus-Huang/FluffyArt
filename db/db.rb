91ce8b4b5ab061baf236b059e28f27eab74d9c54
Titus Huang <titus.the.huang@gmail.com>
2022-09-14 10:17:48 +0930

    tweaked and updated database

--- db/db.rb
+++ db/db.rb
@@ -1,3 +1,4 @@
+
 def run_sql(sql, sql_params = [])
     db = PG.connect(ENV['DATABASE_URL'] || {dbname: ENV['DATABASE_NAME']})
     results = db.exec_params(sql, sql_params)
--- db/schema.sql
+++ db/schema.sql
@@ -11,12 +11,13 @@ CREATE TABLE users(
 );
 
 -- Reset Users table
-
+TRUNCATE TABLE users;
+ALTER SEQUENCE users_id_seq RESTART WITH 1;
 
 -- Create Content table
-CREATE TABLE content(
+CREATE TABLE contents(
     id SERIAL PRIMARY KEY,
-    user_id TEXT,
+    user_id INTEGER,
     post_timestamp TIMESTAMP,
     edit_timestamp TIMESTAMP,
     title TEXT,
@@ -25,7 +26,10 @@ CREATE TABLE content(
     is_text BOOLEAN
 );
 
+-- Reset Content table
+TRUNCATE TABLE contents;
+ALTER SEQUENCE contents_id_seq RESTART WITH 1;
 
 -- Seed example content
-INSERT INTO(user_id, post_timestamp, edit_timestamp, title, content, is_image, is_text)
+INSERT INTO contents(user_id, post_timestamp, edit_timestamp, title, content, is_image, is_text)
 VALUES();
